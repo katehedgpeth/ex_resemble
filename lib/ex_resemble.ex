5a2f81e76429a4b591a619d6b4314ff2834aa0f0
Kate Hedgpeth <katehedgpeth@gmail.com>
2018-06-16 13:34:57 -0400

    initial commit

---
+++ lib/ex_resemble.ex
@@ -0,0 +1,18 @@
+defmodule ExResemble do
+  @moduledoc """
+  Documentation for ExResemble.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExResemble.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_resemble_test.exs
@@ -0,0 +1,8 @@
+defmodule ExResembleTest do
+  use ExUnit.Case
+  doctest ExResemble
+
+  test "greets the world" do
+    assert ExResemble.hello() == :world
+  end
+end
